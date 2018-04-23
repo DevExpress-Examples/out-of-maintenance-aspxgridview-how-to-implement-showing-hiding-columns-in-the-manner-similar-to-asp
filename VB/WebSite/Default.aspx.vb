18ed2d5bca5c038c4b63ac1080ea5b95cb1b1a3a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports DevExpress.Web
+
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub gvData_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		If e.Parameters <> String.Empty Then
+			TryCast(sender, ASPxGridView).Columns(e.Parameters).Visible = False
+			TryCast(sender, ASPxGridView).Columns(e.Parameters).ShowInCustomizationForm = True
+		End If
+	End Sub
+End Class
