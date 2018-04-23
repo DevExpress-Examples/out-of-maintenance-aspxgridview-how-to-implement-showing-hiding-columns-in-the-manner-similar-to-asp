6e001ee40e7dffa5f839d8e57b18e99484c45656
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Web.ASPxGridView;
+
+
+public partial class _Default : System.Web.UI.Page {
+    protected void gvData_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        if (e.Parameters != string.Empty) {
+            (sender as ASPxGridView).Columns[e.Parameters].Visible = false;
+            (sender as ASPxGridView).Columns[e.Parameters].ShowInCustomizationForm = true;
+        }
+    }
+}
